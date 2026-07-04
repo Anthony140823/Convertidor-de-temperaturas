0c734b3ad5013144c0ffff05ce5e1e9193683b60
Anthony140823 <anthonyreyes140803@gmail.com>
2026-07-03 19:38:58 -0500

    Se colocaron las formulas para las converciones

--- main.py
+++ main.py
@@ -3,4 +3,8 @@
 
 Realizar dos funciones para convertir
 grados celsius a fahrenheit y viceversa.
+
+Formulas:
+Celsius a Fahrenheit: °F = (°C * (9/5)) + 32
+Fahrenheit a Celsius: °C = (°F - 32) * (5/9)
 '''
